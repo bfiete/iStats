4dd343c220c407da7c4c222c3211e395e920b3d7
Brian Fiete <bfiete@gmail.com>
2021-09-15 11:20:15 -0700

    utf-8 fix

--- do.sh
+++ do.sh
@@ -8,4 +8,4 @@ git pull
 ../Beef/IDE/dist/BeefBuild -config=Debug
 ../Beef/IDE/dist/BeefBuild -config=Release
 ./build/Debug_Linux64/iStats/iStats
-aws s3 sync html s3://www.simracingstats.com --cache-control 'public, max-age=300'
+aws s3 sync --delete html s3://www.simracingstats.com --cache-control 'public, max-age=300' --content-type 'text/plain; charset=utf-8'
